ebb390e844c4f0d9703175bd9a3a78a055b2d67b
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2010-11-21 15:53:50 -0700

    Reapplied old styles

--- index.php
+++ index.php
@@ -21,7 +21,37 @@
 </head>
 <body>
 
-<?php include_once('resources/UberGallery.php'); $gallery = UberGallery::factory()->createGallery('gallery-images'); ?>
+<?php // include_once('resources/UberGallery.php'); $gallery = UberGallery::factory()->createGallery('gallery-images'); ?>
+
+<?php include_once('resources/UberGallery.php'); $gallery = new UberGallery(); ?>
+
+<!-- Start UberGallery ' . UberGallery::VERSION .' - Copyright (c) ' . date('Y') . ' Chris Kankiewicz (http://www.ChrisKankiewicz.com) -->
+<div id="galleryWrapper">
+    <div id="galleryHeader" class="clearfix">
+        <h1>Uber Gallery</h1>
+    </div>
+    
+    <div id="galleryListWrapper">
+        <ul id="galleryList" class="clearfix">
+            <?php foreach ($gallery->readImageDirectory('gallery-images') as $image): ?>
+                <li><a href="<?php echo $image['file_path']; ?>" title="<?php echo $image['file_title']; ?>" rel="colorbox"><img src="<?php echo $image['thumb_path']; ?>" alt="<?php echo $image['file_title']; ?>"/></a></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    
+    <div id="galleryFooter" class="clearfix">
+        <ul id="galleryPagination">
+            <li class="title">Page 1 of 3</li>
+            <li class="inactive">&lt;</li>
+            <li class="current">1</li>
+            <li><a title="Page 2" href="index.php?page=2">2</a></li>
+            <li><a title="Page 3" href="index.php?page=3">3</a></li>
+            <li><a title="Next Page" href="index.php?page=2">&gt;</a></li>
+          </ul>
+        <div id="credit">Powered by, <a href="http://www.ubergallery.net">UberGallery</a></div>
+    </div>
+</div>
+<!-- End UberGallery - Dual licensed under the MIT & GPL license -->
 
 </body>
 </html>
