182dc16a53502ad4e30ec237eab9f8d08e4d0a6c
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2011-04-27 21:49:57 -0700

    Bumped version to 2.1.0

--- resources/UberGallery.php
+++ resources/UberGallery.php
@@ -18,7 +18,7 @@
 class UberGallery {
     
     // Define application version
-    const VERSION = '2.0.1';
+    const VERSION = '2.1.0';
     
     // Set default config variables
     protected $_cacheExpire = 0;
