0ec293a68e674f8bef5c50f179e4a643bdca7bb9
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2011-07-06 13:50:23 -0700

    Updated jQuery to v1.6.2 and Colorbox to v1.3.17.2

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" type="text/css" href="resources/css/style.css" />
     <link rel="stylesheet" type="text/css" href="resources/css/colorbox.css" />
     
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
     <script type="text/javascript" src="resources/js/jquery.colorbox.js"></script>
     
     <script type="text/javascript">
