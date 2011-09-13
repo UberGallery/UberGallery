da497c30b4d2209bcc68ced7ee0975f0ec7c860b
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2011-09-13 14:22:52 -0700

    Removed hard-coded index.php from pagination in the createGallery() function

--- resources/UberGallery.php
+++ resources/UberGallery.php
@@ -174,7 +174,7 @@ class UberGallery {
                 
             if ($gallery['stats']['current_page'] > 1) {
                 $previousPage = $gallery['stats']['current_page'] - 1;
-                echo "                <li><a title=\"Previous Page\" href=\"index.php?page={$previousPage}\">&lt;</a></li>" . PHP_EOL;
+                echo "                <li><a title=\"Previous Page\" href=\"?page={$previousPage}\">&lt;</a></li>" . PHP_EOL;
             } else {
                 echo '                <li class="inactive">&lt;</li>' . PHP_EOL;
             }
@@ -183,13 +183,13 @@ class UberGallery {
                 if($x == $gallery['stats']['current_page']) {
                     echo "                    <li class=\"current\">{$x}</li>" . PHP_EOL;
                 } else {
-                    echo "                    <li><a title=\"Page {$x}\" href=\"index.php?page={$x}\">{$x}</a></li>" . PHP_EOL;
+                    echo "                    <li><a title=\"Page {$x}\" href=\"?page={$x}\">{$x}</a></li>" . PHP_EOL;
                 }
             }
                 
             if ($gallery['stats']['current_page'] < $gallery['stats']['total_pages']) {
                 $nextPage = $gallery['stats']['current_page'] + 1;
-                echo "                <li><a title=\"Next Page\" href=\"index.php?page={$nextPage}\">&gt;</a></li>" . PHP_EOL;
+                echo "                <li><a title=\"Next Page\" href=\"?page={$nextPage}\">&gt;</a></li>" . PHP_EOL;
             } else {
                 echo '                <li class="inactive">&gt;</li>' . PHP_EOL;
             }
