495d289a5dab8e4a866c0c6c774a993a15a854d2
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2011-09-13 14:30:33 -0700

    Updated jqery includes to v1.6.4

--- resources/themes/uber-blue/index.php
+++ resources/themes/uber-blue/index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/style.css" />
     <?php echo $gallery->getColorboxStyles(5); ?>
     
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
     <script type="text/javascript" src="resources/colorbox/jquery.colorbox.js"></script>
     
     <script type="text/javascript">
