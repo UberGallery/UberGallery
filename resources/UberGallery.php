776a2cc008daef4b2ec840417bef2cd082778b62
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2012-01-30 09:44:02 -0700

    Updated copyright date in UberGallery class doc block

--- resources/UberGallery.php
+++ resources/UberGallery.php
@@ -13,7 +13,7 @@
  * More info available at http://www.ubergallery.net
  * 
  * @author Chris Kankiewicz (http://www.chriskankiewicz.com)
- * @copyright 2011 Chris Kankiewicz
+ * @copyright 2012 Chris Kankiewicz
  */
 class UberGallery {
     
