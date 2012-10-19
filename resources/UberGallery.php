314faa99b2bd4930ca4437cfc22a5f968796d36c
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2012-10-18 18:10:48 -0700

    Bumpped version to 2.4.2

--- resources/UberGallery.php
+++ resources/UberGallery.php
@@ -17,7 +17,7 @@
 class UberGallery {
     
     // Define application version
-    const VERSION = '2.4.2-dev';
+    const VERSION = '2.4.2';
     
     // Reserve some variables
     protected $_config     = array();
