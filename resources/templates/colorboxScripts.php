8faa8dbdd87b6dd6ddfa25e294327f592733d993
peraburek <peraburek@users.noreply.github.com>
2015-12-07 23:59:45 +0100

    change click behavior - previous / next
    This code will change click behavior
    click on the left part of the image will go to the previous
    click on the right part of the image will go to the next

--- resources/templates/colorboxScripts.php
+++ resources/templates/colorboxScripts.php
@@ -4,4 +4,19 @@
     $(document).ready(function(){
         $("a[rel='colorbox']").colorbox({maxWidth: "90%", maxHeight: "90%", opacity: ".5"});
     });
+	/*
+The code below will change click behavior
+click on the left part of the image will go to the previous
+click on the right part of the image will go to the next
+credit - Jack Moore - http://www.jacklmoore.com/colorbox/
+*/
+	jQuery(document).on('cbox_complete', function(){
+	jQuery('.cboxPhoto').off('click.cbox').on('click.cbox', function(e){
+		if (e.offsetX < (e.target.width / 2)) {
+			jQuery.colorbox.prev();
+		} else {
+			jQuery.colorbox.next();
+		}
+	});
+});
 </script>
