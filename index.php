61bc9072c933c1c85a88c70f933dcdba449f049a
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2018-03-20 13:09:13 -0700

    Removed 'routerCacheFile' configuration option from app instantiation array

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 $app = new \Slim\App([
     'settings' => array_merge([
-        'routerCacheFile' => __DIR__ . '/cache/routes.cache.php',
         'albums' => (include __DIR__ . '/config/albums.php') ?: [],
         'cache' => (include __DIR__ . '/config/cache.php') ?: []
     ], (include __DIR__ . '/config/app.php') ?: []),
