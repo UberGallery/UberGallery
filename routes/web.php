e2d1708b0c7309a018d25f1a37c6f3012ac34e49
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2018-03-20 13:07:44 -0700

    Added CacheMiddleware to image and thumbnail routes

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,15 @@
 
 $app->get('/', App\Controllers\GalleryController::class)->setName('index');
 
-$app->group('/{album}', function () {
-    $this->get('/', App\Controllers\AlbumController::class)->setName('album');
-    // $this->get('/{page:[0-9]+}', App\Controllers\AlbumController::class)->setName('album');
-    $this->get('/{image}', App\Controllers\ImageController::class)->setName('image');
-    $this->get('/thumbnail/{image}', App\Controllers\ThumbnailController::class)->setName('thumbnail');
+$app->group('/{album}', function ($app) {
+    $app->get('/[{page:[0-9]+}]', App\Controllers\AlbumController::class)
+        ->setName('album');
+
+    $app->get('/{image}', App\Controllers\ImageController::class)
+        ->add(new App\Middleware\CacheMiddleware($app->getContainer()))
+        ->setName('image');
+
+    $app->get('/thumbnail/{image}', App\Controllers\ThumbnailController::class)
+        ->add(new App\Middleware\CacheMiddleware($app->getContainer()))
+        ->setName('thumbnail');
 });
