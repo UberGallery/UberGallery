19e726cb19e67214b77b54fe89734d9e544be846
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2018-04-06 18:38:37 -0700

    Throw an exception when using an invlid cache configuration

--- app/Services/CacheService.php
+++ app/Services/CacheService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use PHLAK\Stash;
+use App\Exceptions\InvalidConfigurationException;
 
 class CacheService extends Service
 {
@@ -14,8 +15,12 @@ class CacheService extends Service
     public function register()
     {
         $this->bind('cache', function ($container) {
-            $driver = $container->config->get('cache.driver', 'file');
+            $driver = $container->config->get('cache.driver');
             $config = $container->config->get("cache.drivers.{$driver}");
+
+            if (! isset($driver, $config)) {
+                throw new InvalidConfigurationException('The cache configuraton is invalid');
+            }
 
             return Stash\Cache::$driver($config);
         });
