2bc5050d086986b5b2483740feacca6079409115
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-07-13 17:46:16 -0700

    Refactored theme and implemented initial image modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,22 @@ module.exports = {
     purge: [],
     theme: {
         extend: {
-            inset: { '-2': '-2px' }
+            fontFamily: {
+                poiretone: ['Poiret One', 'sans-serif']
+            },
+            inset: { '-2': '-2px' },
+            spacing: {
+                '100': '100%'
+            },
+            textColor: {
+                github: '#171515',
+                spectrum: '#7B16FF',
+                twitter: '#1DA1F2'
+            }
         },
     },
     variants: {
-        inset: ['responsive', 'hover'],
+        inset: ['responsive', 'hover']
     },
-    plugins: [],
-}
+    plugins: []
+};
